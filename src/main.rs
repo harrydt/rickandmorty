66a15acd54ae914dd3a116dd508af7b6a7e72858
Harry Tran <huydtran19@gmail.com>
2022-10-22 15:41:14 -0400

    init PoC that allows get-all operations on predefined resources (character, episode,location)

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+// use std::collections::HashMap;
+use clap::{Parser, ValueEnum};
+use core::fmt;
+use std::time::Duration;
+
+const URL: &str = "https://rickandmortyapi.com/api/";
+const TIMEOUT: u64 = 3;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(value_enum)]
+    resource: Resource,
+}
+
+#[derive(ValueEnum, Debug, Clone)]
+enum Resource {
+    Character,
+    Location,
+    Episode,
+}
+
+impl fmt::Display for Resource {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", format!("{:?}", self).to_lowercase())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+
+    // Construct API endpoint based on the input resource
+    let url = URL.to_string() + &args.resource.to_string();
+
+    let client = reqwest::Client::new();
+    let resp = client
+        .get(url)
+        .header("Accept", "text/plain")
+        .timeout(Duration::from_secs(TIMEOUT))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{:}", resp);
+    Ok(())
+}
